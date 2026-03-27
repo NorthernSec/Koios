1085811670ed4bbc5fe5415693046781cbf7f97a
Pieter-Jan <Pieter-Jan@pidgey.net>
2026-03-27 15:25:16 +0100

    update csp_report to allow for filtering

--- app/csp_report/views.py
+++ app/csp_report/views.py
@@ -1,10 +1,23 @@
 import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from pprint import pprint
+
+IGNORE_SOURCE_FILES = [
+    'sandbox eval code',
+]
 
 @csrf_exempt
+@require_http_methods(["POST"])
 def csp_report(request):
-    if request.method == "POST":
+    try:
         data = json.loads(request.body.decode("utf-8"))
-        print(data)
+    except Exception as e:
+        #TODO: Logging
+        return HttpResponse(status=500)
+    if data.get('csp-report', {}).get("source-file", '').lower() in IGNORE_SOURCE_FILES:
+        #TODO: Logging (debug)
+        return HttpResponse(status=204)
+    pprint(data)
     return HttpResponse(status=204)
